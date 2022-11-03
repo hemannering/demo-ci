bbec250e205d6f74662b6699057144218269b40f
hemannering <55000580+hemannering@users.noreply.github.com>
2022-11-03 15:31:12 -0400

    Changing function addition

--- src/main/java/br/ufmg/dcc/SimpleCalculator.java
+++ src/main/java/br/ufmg/dcc/SimpleCalculator.java
@@ -1,9 +1,10 @@
+b/src/main/java/br/ufmg/dcc/SimpleCalculator.java
 package br.ufmg.dcc;
 
 public class SimpleCalculator {
 
   public int addition(int x, int y) {
-    return x + y;
+    return x + y + 1;
   }
 
   public int subtraction(int x, int y) {
